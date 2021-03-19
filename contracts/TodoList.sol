0c9e000b8269131dc954508d40d73ff9b440ff1b
Kshitij Dhyani <wimpywarlord@gmail.com>
2021-03-19 14:03:00 +0530

    added base structure

---
+++ contracts/TodoList.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.4.20;
+
+contract TodoList {
+    // ALL THE CODE GOES HERE
+    uint public taskCount = 0; //STATE VARIABLES, are WRITTEN TO THE BLOCKCHAIN
+    // STATE CHANGES EVERYTIME STATE VARIABLE CHANGES
+
+
+}
